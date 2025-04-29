3f74952ce3521cc2d687c0766f64c577c584fa32
losakovvitalik <losakovgithu@gmail.com>
2025-04-29 16:54:49 +0300

    init

---
+++ src/api/time-entry/controllers/time-entry.ts
@@ -0,0 +1,7 @@
+/**
+ * time-entry controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::time-entry.time-entry');
---
+++ src/api/time-entry/routes/time-entry.ts
@@ -0,0 +1,7 @@
+/**
+ * time-entry router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::time-entry.time-entry');
