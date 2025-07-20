e33ff9e7422dbf16ef285d121465fcd7087fd3b4
losakovvitalik <losakovgithub@gmail.com>
2025-07-20 11:32:59 +0300

    fix(project): :ambulance: сделал, чтоб при создании проекта указывался владелец и участник

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -2,6 +2,17 @@
  * project controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
+import Context from '../../../shared/utils/context';
 
-export default factories.createCoreController('api::project.project');
+export default factories.createCoreController('api::project.project', ({ strapi }) => ({
+  async create(ctx) {
+    const context = new Context(ctx);
+    ctx.request.body.data.owner = context.getUserId();
+    ctx.request.body.data.members = context.getUserId();
+
+    // Proceed with the default create logic
+    const response = await super.create(ctx);
+    return response;
+  },
+}));
