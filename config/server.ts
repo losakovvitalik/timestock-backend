9c6beb9a833fb1ec51bb97e083794f941b955810
Виталий <losakovgithub@gmail.com>
2025-10-15 21:26:54 +0300

    chore(config): добавил параметр proxy в конфиг

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,12 @@
-import cronTasks from "./cron-tasks";
+import cronTasks from './cron-tasks';
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  secure: true,
+  proxy: {
+    koa: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
