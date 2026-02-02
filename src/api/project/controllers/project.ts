2a268996fec84eeeefe797ebf8d6e36662542516
losakovvitalik <losakovgithub@gmail.com>
2026-02-03 00:19:31 +0300

    feat(project): добавил проверку на id владельца при удалении проекта

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -3,6 +3,7 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { sendForbiddenError, sendNotFoundError } from '../../../shared/lib/response';
 import Context from '../../../shared/utils/context';
 
 export default factories.createCoreController('api::project.project', ({ strapi }) => ({
@@ -11,8 +12,29 @@ export default factories.createCoreController('api::project.project', ({ strapi 
     ctx.request.body.data.owner = context.getUserId();
     ctx.request.body.data.members = context.getUserId();
 
-    // Proceed with the default create logic
     const response = await super.create(ctx);
+    return response;
+  },
+
+  async delete(ctx) {
+    const context = new Context(ctx);
+    const userId = context.getUserId();
+    const { id } = ctx.params;
+
+    const projects = await strapi.documents('api::project.project').findOne({
+      documentId: id,
+      populate: { owner: true },
+    });
+
+    if (!projects) {
+      return sendNotFoundError({ message: 'Project not found' });
+    }
+
+    if (projects.owner.id !== userId) {
+      return sendForbiddenError({ message: 'You can only delete your own projects' });
+    }
+
+    const response = await super.delete(ctx);
     return response;
   },
 }));
